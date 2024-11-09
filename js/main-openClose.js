8a35f0b39654788bd75f650eb6ac5050693a1094
krib00 <kristijan.boben30@gmail.com>
2024-11-10 00:43:53 +0100

    Transitions - CE
    Changes: 
    - Animations completed for both forms.
    - Added a transition for a blended background when closing a form.

--- js/main-openClose.js
+++ js/main-openClose.js
@@ -1,8 +1,10 @@
 const signUp = document.querySelector('.signupFunction');
 const animationS = document.querySelector('.glavniS');
+const animationL = document.querySelector('.glavniL');
 const glavni = document.querySelector('.glavni');
 const login = document.querySelector('.loginFunction');
 function openSignup() {
+    signUp.style.opacity = "1";
     signUp.style.display = "flex";
     animationS.classList.add("showS");
     animationS.classList.remove("hideS");
@@ -10,32 +12,42 @@ function openSignup() {
     glavni.style.paddingRight = "17px"; // ostane ista pozicija, kadar izgine overflow, overflow width=17px
 }
 function closeSignup() {
-    signUp.classList.add("transS");
+    signUp.style.opacity = "0";
     animationS.classList.remove("showS");
-    animationS.classList.add("hideS");
+    animationS.classList.add("hideS"); 
     setTimeout(() => {
         signUp.style.display = "none";
         glavni.style.overflow = "auto scroll";
         glavni.style.paddingRight = "0";
         refresh();
     }, 300); // 300ms = 0.3s ... display none zamaknjen za 0.3 s, da se bo animacija izvedla prej
-
-} function openLogin() {
+}
+function openLogin() {
+    login.style.opacity = "1";
     login.style.display = "flex";
     glavni.style.overflow = "hidden";
     glavni.style.paddingRight = "17px";
+    animationL.classList.add("showL");
+    animationL.classList.remove("hideL"); 
 }
 function closeLogin() {
-    login.style.display = "none";
-    glavni.style.overflow = "auto scroll";
-    glavni.style.paddingRight = "0";
-    refresh();
+    login.style.opacity = "0";
+    animationL.classList.remove("showL");
+    animationL.classList.add("hideL"); 
+    setTimeout(() => {
+        glavni.style.overflow = "auto scroll";
+        glavni.style.paddingRight = "0";
+        login.style.display = "none";
+        refresh();
+    }, 300); 
 }
-
 function dontHaveAnAccount() {
     login.style.display = "none";
     document.querySelectorAll(".comboBoxes select").forEach(select => {
         select.selectedIndex = 0;
     });
     signUp.style.display = "flex";
+    signUp.style.opacity = "1";
+    animationS.classList.add("showS");
+    animationS.classList.remove("hideS");
 }
